8fbb776801c215549562ef90ec7f5406561190c0
SergioRuiz <sergioruizrock@gmail.com>
2024-03-11 21:58:32 -0300

    configuramos un puerto para el repositorio

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ import express from 'express'
 console.log('bienvenidos c74i')
 
 // 1 - configurar un puerto
+const app = express()
+
+app.set("puerto",4000) // process.env.PORT || 4000 para traer el puerto de entorno o 400 por defecto
+
+
+app.listen(app.get("puerto"),()=>{ //bajo este puerto realiza esta funcion
+    console.log("estoy en el puerto "+app.get("puerto"))
+})
+
 
 // 2 - configurar los middlewares
 
